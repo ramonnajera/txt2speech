4d42eabcb58dd30e2b0ac6eee79289b2923be071
Ramon Najera <ramonnajera@hotmail.com>
2024-05-06 15:06:28 -0600

    Se agrega configuracion y textos

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from os import getenv
-from app.api import app
 import uvicorn
-from dotenv import load_dotenv
+from configparser import ConfigParser
+from typing import Optional
 
-load_dotenv('.env')
+def get_config_value(config: ConfigParser, section: str, key: str, default: Optional[str] = None) -> str:
+    try:
+        return config.get(section, key)
+    except:
+        return default
+
+def main():
+    config = ConfigParser()
+    config.read('config/config.ini')
+
+    host = get_config_value(config, 'server', 'host', '0.0.0.0')
+    port = get_config_value(config, 'server', 'port', '8000')
+
+    try:
+        uvicorn.run("app.api:app", host=host, port=int(port), reload=True)
+    except ValueError:
+        print(f"Error: Invalid port number {port}")
 
 if __name__ == "__main__":
-    host = getenv('HOST', '0.0.0.0')
-    port = int(getenv('PORT', 8000))
-    uvicorn.run(app, host=host, port=port, reload=True)
+    main()
